c1a23da59b81cc045223ad92b04051636bd08cba
UreshaSew <vpusewwandi@gmail.com>
2019-08-20 09:00:05 +0530

    init

--- src/test/java/Test4.java
+++ src/test/java/Test4.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class Test4 {
     public static void main(String[]args){
         System.setProperty("webdriver.chrome.driver","C:\\selenium\\crome\\chromedriver.exe");
@@ -16,11 +17,26 @@ public class Test4 {
 
         //There are two methods to get web address
         // method 1    -     driver.get("https://www.google.lk/");
+        //driver.navigate().to("https://www.google.lk/"); // method 2
 
-        driver.navigate().to("https://www.google.lk/"); // method 2
-        driver.findElement(By.name("q")).sendKeys("cat");
+     //   driver.findElement(By.name("q")).sendKeys("cat");
         //driver.findElement(By.name("q")).sendKeys("Selenium");
-        driver.findElement(By.id("btnK")).click();
+       // driver.findElement(By.id("btnK")).click();
+
+
+        driver.navigate().to("https://www.testandquiz.com/selenium/testing.html");
+        String sampleText = driver.findElement(By.className("col-md-12")).getText();
+        System.out.println(sampleText);
+       // driver.findElement(By.linkText("This is a link")).click();
+        driver.findElement(By.id("fname")).sendKeys("JavaTpoint");
+        driver.findElement(By.id("fname")).clear();
+        driver.findElement(By.id("idOfButton")).click();
+
+        driver.findElement(By.id("male")).click();
+        driver.findElement(By.className("Automation")).click();
+       driver.findElement(By.id("testingDropdown")).click();
+       // driver.findElement(By.cssSelector("input.Database")).click();
+
 
 
     }
