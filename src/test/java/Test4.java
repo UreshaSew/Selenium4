ecc7dcdc00262aa18048a65139b6ded54a40a88b
Uresha VidanaPathirana <41063078+UreshaSew@users.noreply.github.com>
2020-02-26 14:46:53 +0530

    Update Test4.java

--- src/test/java/Test4.java
+++ src/test/java/Test4.java
@@ -9,22 +9,7 @@ public class Test4 {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-       // driver.get("https://www.facebook.com");
-       // driver.findElement(By.id("email")).sendKeys("vpusewwandi@gmail.com");
-       // driver.findElement(By.id("pass")).sendKeys("uresha19");
-       // driver.findElement(By.id("loginbutton")).click();
-        //System.out.println("logged in successfully");
-
-        //There are two methods to get web address
-        // method 1    -     driver.get("https://www.google.lk/");
-        //driver.navigate().to("https://www.google.lk/"); // method 2
-
-     //   driver.findElement(By.name("q")).sendKeys("cat");
-        //driver.findElement(By.name("q")).sendKeys("Selenium");
-       // driver.findElement(By.id("btnK")).click();
-
-
-        driver.navigate().to("https://www.testandquiz.com/selenium/testing.html");
+    driver.navigate().to("https://www.testandquiz.com/selenium/testing.html");
         String sampleText = driver.findElement(By.className("col-md-12")).getText();
         System.out.println(sampleText);
        driver.findElement(By.linkText("This is a link")).click();
@@ -38,7 +23,19 @@ public class Test4 {
         driver.findElement(By.className("Automation")).click();
         driver.findElement(By.id("testingDropdown")).click();
        // driver.findElement(By.cssSelector("input.Database")).click();
+ // driver.get("https://www.facebook.com");
+       // driver.findElement(By.id("email")).sendKeys("vpusewwandi@gmail.com");
+       // driver.findElement(By.id("pass")).sendKeys("uresha19");
+       // driver.findElement(By.id("loginbutton")).click();
+        //System.out.println("logged in successfully");
 
+        //There are two methods to get web address
+        // method 1    -     driver.get("https://www.google.lk/");
+        //driver.navigate().to("https://www.google.lk/"); // method 2
+
+     //   driver.findElement(By.name("q")).sendKeys("cat");
+        //driver.findElement(By.name("q")).sendKeys("Selenium");
+       // driver.findElement(By.id("btnK")).click();
 
 
     }
